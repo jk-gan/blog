008bc5e5dc5323a5af6745382b4d4a2125e6a82c
Jun Kai <kuhn96@gmail.com>
2016-03-12 15:30:01 +0800

    Make it DRY

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
@@ -6,7 +7,6 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,11 +23,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to @post
     else
@@ -36,13 +34,17 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path
   end
 
   private
-  def post_params
-    params.require(:post).permit(:title, :content)
-  end
+
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 end
